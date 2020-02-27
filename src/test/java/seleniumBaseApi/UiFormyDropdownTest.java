9b5bbd519d1e6046ad0a6ef8680a7d9f5caa3b5e
vladimirAIluhin <vladimir.a.iluhin@gmail.com>
2020-02-28 00:04:50 +0300

    added solution for homework#2

--- src/test/java/seleniumBaseApi/UiFormyDropdownTest.java
+++ src/test/java/seleniumBaseApi/UiFormyDropdownTest.java
@@ -1,15 +1,22 @@
 package seleniumBaseApi;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class UiFormyDropdownTest {
     private WebDriver chromeDriver;
     private static final String BASE_URL = "https://formy-project.herokuapp.com";
+    private static final List<String> hrefAttributesForDropdownItems = Arrays.asList(
+            "/autocomplete", "/buttons", "/checkbox", "/datepicker", "/dragdrop", "/dropdown", "/enabled", "/fileupload",
+            "/filedownload", "/keypress", "/modal", "/scroll", "/radiobutton", "/switch-window", "/switch-window",
+            "/form");
+
 
     @BeforeClass
     public void createDriver() {
@@ -22,18 +29,23 @@ public class UiFormyDropdownTest {
         chromeDriver.quit();
     }
 
-    @AfterMethod
-    public void waitABit() {
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+//    @AfterMethod
+//    public void waitABit() {
+//        try {
+//            Thread.sleep(500);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    @Test
+    public void testUrlFromDropdown() {
+        chromeDriver.get(BASE_URL + "/dropdown");
+        for (String href : hrefAttributesForDropdownItems) {
+            chromeDriver.findElement(By.cssSelector("button.dropdown-toggle")).click();
+            chromeDriver.findElement(By.cssSelector(".dropdown-menu.show>.dropdown-item[href='" + href + "']")).click();
+            Assert.assertEquals(chromeDriver.getCurrentUrl(), BASE_URL + href, "URL should match.");
+            chromeDriver.get(BASE_URL + "/dropdown");
         }
     }
-
-    @BeforeMethod
-    public void openDropdownPage() {
-        chromeDriver.get(BASE_URL + "/dropdown");
-    }
-
 }
